f78ccba185aaf98ea16af6f2eff2e76347508231
Tsutomu Yoshida <tsuntsunkobi@icloud.com>
2025-09-26 17:54:32 +0900

    adapted Swift Concurrency. reimplemented STEP entity inverse attributes as derived attribute types.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
 	name: "SwiftSDAIap242",
 	platforms: [
-		.macOS(.v10_15),
+		.macOS(.v15)
 	],
 	products: [
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
