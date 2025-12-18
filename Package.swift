33b20a260ed7ba9ba0b0e15e3a85e5b8ac5ad55c
Tsutomu Yoshida <tsuntsunkobi@icloud.com>
2025-12-18 16:00:11 +0900

    alternative implementation proposal for FUNCTION using_items(). regenerated swift codes using updated exp2swift for InitializableByVoid protocol conformance.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
 	name: "SwiftSDAIap242",
 	platforms: [
-		.macOS(.v15)
+		.macOS(.v26)
 	],
 	products: [
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
